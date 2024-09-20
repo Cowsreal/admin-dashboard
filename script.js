e48b7c63732803d9088dd1b4cb9b6099a50a4ac4
Cowsreal <markthezhang@gmail.com>
2024-09-20 02:17:30 -0700

    Finished Site

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let searchBar = document.getElementById('searchQuery');
 let searchButton = document.getElementById('searchButton');
 let glass = searchButton.querySelector('svg');
+let actionButtons = document.getElementById('actionButtons').querySelectorAll('button');
+let recommendedPics = document.querySelectorAll('.recommendedPic');
 
 function focus(element)
 {
@@ -8,13 +10,16 @@ function focus(element)
     element.style.backgroundColor = '#18444a';
     element.style.boxShadow = '0 0 10px #00ffcc, 0 0 40px #009b7c, 0 0 80px #00765f';
     element.style.border = '2px solid #00ffcc';
+    element.style.color = '#00ffcc';
 }
 
 function unfocus(element)
 {
     element.style.backgroundColor = '#368F8B';
     element.style.boxShadow = 'none';
-    element.style.border = '1px solid #b6ab87';
+    element.style.border = '1px solid #276764';
+    element.style.color = '#ffffff';
+    element.style.boxShadow = '0.15rem 0.15rem 0.7rem rgba(0, 0, 0, 0.5)';
 }
 
 
@@ -28,4 +33,19 @@ searchButton.addEventListener('mouseover', function()
     glass.style.fill = '#00ffcc';
 }); 
 
-searchButton.addEventListener('mouseout', function(){unfocus(searchButton);}); 
+searchButton.addEventListener('mouseout', function()
+{
+    unfocus(searchButton);
+    glass.style.fill = '#F3DFC1';
+}); 
+
+for(let button of actionButtons)
+{
+    button.addEventListener('mouseover', function(){focus(button);});
+    button.addEventListener('mouseout', function(){unfocus(button);});
+}
+
+for(let pic of recommendedPics)
+{
+    pic.style.width = `${pic.offsetHeight}px`;
+}
